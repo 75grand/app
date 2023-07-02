fd0faa0728f8f35653a0f4fbc423798a54c77e6c
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-02 00:28:25 -0700

    Send user with Sentry errors

--- components/User.tsx
+++ components/User.tsx
@@ -2,11 +2,12 @@ import { Image } from 'expo-image';
 import { Platform, TouchableOpacity } from 'react-native';
 import tw from '../lib/tailwind';
 import Button from './Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { get } from '../lib/http';
 import type { User } from '../lib/models/user';
 import { useRouter } from 'expo-router';
 import { login } from '../lib/login';
+import * as Sentry from '@sentry/react-native';
 
 export default function User() {
     const router = useRouter();
@@ -14,6 +15,18 @@ export default function User() {
     const [loading, setLoading] = useState(false);
 
     if(Platform.OS === 'android') return;
+
+    useEffect(() => {
+        if(user === null) {
+            Sentry.setUser(null);
+        } else {
+            Sentry.setUser({
+                id: user.id.toString(),
+                username: user.name,
+                email: user.email
+            });
+        }
+    }, [user]);
 
     async function handlePress() {
         setLoading(true);
