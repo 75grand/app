879fc03f3d7b3e54050a99bc433c88db9d1d8ca9
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-15 23:02:40 -0700

    Bare minimum hours page

--- src/screens/Hours.tsx
+++ src/screens/Hours.tsx
@@ -1,5 +1,8 @@
 import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
-import { Text } from 'react-native';
+import HoursCard from '../components/home/HoursCard';
+import Grid from '../components/Grid';
+import { SafeAreaView, ScrollView } from 'react-native';
+import tw from '../helpers/tailwind';
 
 export const screenOptions: NativeStackNavigationOptions = {
     title: 'Campus Hours'
@@ -7,6 +10,12 @@ export const screenOptions: NativeStackNavigationOptions = {
 
 export default function Hours() {
     return (
-        <Text>Hours</Text>
+        <SafeAreaView>
+            <ScrollView style={tw('h-full')}>
+                <Grid columns={1} style={tw('p-3')}>
+                    <HoursCard/>
+                </Grid>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
