8c8e902bc39380c2b76ee17f325748fa86dcdbc9
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-19 23:47:35 -0700

    Add description to InputLabel

--- src/components/InputLabel.tsx
+++ src/components/InputLabel.tsx
@@ -3,13 +3,18 @@ import tw from '../helpers/tailwind';
 
 interface Props {
     text: string,
+    description?: string,
     children: React.ReactNode
 }
 
-export default function InputLabel({ text, children }: Props) {
+export default function InputLabel({ text, description, children }: Props) {
     return (
         <View style={tw('gap-2')}>
-            <Text style={tw('font-semibold ml-1')}>{text}</Text>
+            <View style={tw('ml-1')}>
+                <Text style={tw('font-semibold')}>{text}</Text>
+                {description && <Text style={tw('text-gray-400')}>{description}</Text>}
+            </View>
+
             <View>{children}</View>
         </View>
     );
