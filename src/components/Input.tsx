c3c962be7cf260252a50176e52c60e135c301d0d
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-19 23:47:52 -0700

    Add prefix/suffix to Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,22 +2,31 @@ import { TextInput, TextInputProps, View, Text } from 'react-native';
 import tw, { color } from '../helpers/tailwind';
 
 interface Props {
-    error?: string
+    error?: string,
+    prefix?: string,
+    suffix?: string
 }
 
 export default function Input(props: TextInputProps & Props) {
+    const shared = tw('text-base leading-5 py-2');
+
     return (
         <View style={tw('gap-2')}>
-            <TextInput
-                {...props}
-                placeholderTextColor={color('text-gray-500/50')}
-                style={tw(
-                    'px-3 py-2 text-base leading-5 w-full',
-                    'bg-white rounded-lg border border-black/10',
-                    props.error && 'border-red text-red',
-                    props.multiline && 'min-h-16 max-h-48'
-                )}
-            />
+            <View style={tw('bg-white rounded-lg px-3 border border-black/10 flex-row')}>
+                {props.prefix && <Text style={shared}>{props.prefix}</Text>}
+
+                <TextInput
+                    {...props}
+                    placeholderTextColor={color('text-gray-500/50')}
+                    style={tw(
+                        shared, 'w-full',
+                        props.error && 'border-red text-red',
+                        props.multiline && 'min-h-16 max-h-48'
+                    )}
+                />
+
+                {props.suffix && <Text style={shared}>{props.suffix}</Text>}
+            </View>
 
             {props.error &&
                 <Text style={tw('font-semibold ml-1 text-red')}>{props.error}</Text>}
