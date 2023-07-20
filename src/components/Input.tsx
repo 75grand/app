1aef2fb4e5dd2cb2565ed8657e5e6e869d0c1b0d
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-19 23:38:20 -0700

    Add error prop to Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,26 @@
-import { Ionicons } from '@expo/vector-icons';
-import { TextInput, TextInputProps, View } from 'react-native';
-import tw from '../helpers/tailwind';
+import { TextInput, TextInputProps, View, Text } from 'react-native';
+import tw, { color } from '../helpers/tailwind';
 
-export default function Input(props: TextInputProps & { search?: boolean }) {
+interface Props {
+    error?: string
+}
+
+export default function Input(props: TextInputProps & Props) {
     return (
-        <View style={tw('gap-2 bg-white rounded-lg border border-black/10 flex flex-row items-center')}>
-            {props.search && <Ionicons style={tw('text-gray-400 pl-3')} name="search" size={18}/>}
-
+        <View style={tw('gap-2')}>
             <TextInput
-                style={tw('pr-3 py-2 text-base leading-5 w-full', props.search || 'pl-3')}
                 {...props}
+                placeholderTextColor={color('text-gray-500/50')}
+                style={tw(
+                    'px-3 py-2 text-base leading-5 w-full',
+                    'bg-white rounded-lg border border-black/10',
+                    props.error && 'border-red text-red',
+                    props.multiline && 'min-h-16 max-h-48'
+                )}
             />
+
+            {props.error &&
+                <Text style={tw('font-semibold ml-1 text-red')}>{props.error}</Text>}
         </View>
     );
 }
