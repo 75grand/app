b685b130235071ddef65d02a78fa817eb4c33b75
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-27 20:59:52 -0700

    Suppress type error

--- src/components/menu/FoodAlertsPromo.tsx
+++ src/components/menu/FoodAlertsPromo.tsx
@@ -15,7 +15,10 @@ export default function FoodAlertsPromo({ onDismiss }: { onDismiss: () => void }
             <Text style={tw('text-white text-base leading-snug mb-4')}>75grand can notify you when your favorite foods are served! Notifications will be delivered silently the night before.</Text>
 
             <View style={tw('flex flex-row gap-3')}>
-                <Button onPress={() => navigation.navigate('HomeTab')} text="Add Favorite Foods" color="light"/>
+                <Button onPress={() => {
+                    // @ts-expect-error
+                    navigation.navigate('HomeTab');
+                }} text="Add Favorite Foods" color="light"/>
                 <Button onPress={onDismiss} text="Dismiss" color="faint-white"/>
             </View>
         </Card>
