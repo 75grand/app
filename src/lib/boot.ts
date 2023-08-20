d4f24d43f1587c7a78776565055d94190779055b
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-19 22:36:09 -0700

    Check user on app launch

--- src/lib/boot.ts
+++ src/lib/boot.ts
@@ -1,8 +1,11 @@
 import { Platform, UIManager } from 'react-native';
 import { registerAndroidNotifChannel, setNotifHandler, syncNotifToken } from './notifications';
 import { loadTokenFromDisk } from './user/token-store';
-import { loadUserFromDisk } from './user/user-store';
+import { $user, loadUserFromDisk } from './user/user-store';
 import { loadSettingsFromDisk } from './user/settings-store';
+import { fetchUser } from './api/api';
+import { HttpStatusCode } from 'axios';
+import { logout } from './api/login';
 
 /**
  * Code that should run before the app loads. The splash screen will
@@ -12,6 +15,15 @@ export async function boot(): Promise<true> {
     await loadTokenFromDisk();
     await loadUserFromDisk();
     await loadSettingsFromDisk();
+
+    try {
+        const user = await fetchUser();
+        $user.set(user);
+    } catch(error) {
+        if(error.response?.status === HttpStatusCode.Unauthorized) {
+            await logout();
+        }
+    }
 
     setNotifHandler();
     registerAndroidNotifChannel();
