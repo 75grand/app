44b94be65691c9bc06c1ac76d926e0f0bc521a12
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-23 11:18:29 -0700

    Fix MacPass scanner needing to be reopened after permission grant

--- src/screens/ScanMacPass.tsx
+++ src/screens/ScanMacPass.tsx
@@ -2,8 +2,8 @@ import { useNavigation, useRoute } from '@react-navigation/native';
 import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import { LogBox, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Alert, LogBox, View } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import tw from '../lib/tailwind';
 import { zMacPass } from '../lib/types/utils';
@@ -29,10 +29,21 @@ export default function ScanMacPass() {
     const navigation = useNavigation();
     const { setMacPass } = useRoute().params as any;
 
+    const [hasPermission, setHasPermission] = useState(false);
+
     useEffect(() => {
         (async () => {
             const { status } = await BarCodeScanner.requestPermissionsAsync();
-            if(status === 'denied') navigation.goBack();
+
+            if(status === 'denied') {
+                Alert.alert(
+                    'Camera permissions required',
+                    'Please allow access to your camera to scan your MacPass'
+                );
+                navigation.goBack();
+            } else {
+                setHasPermission(true);
+            }
         })();
     }, []);
 
@@ -42,6 +53,8 @@ export default function ScanMacPass() {
             navigation.goBack();
         }
     }
+
+    if(!hasPermission) return;
 
     return (
         <>
