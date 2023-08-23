232e231e7a604ef69091f7a074d3e884c54608f3
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-23 00:14:51 -0700

    Use TouchableScale instead of TouchableOpacity for floating action button

--- src/components/FloatingCircleButton.tsx
+++ src/components/FloatingCircleButton.tsx
@@ -1,6 +1,7 @@
 import { Ionicons } from '@expo/vector-icons';
-import { TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
 import tw from '../lib/tailwind';
+import TouchableScale from './TouchableScale';
 
 interface Props {
     icon: keyof typeof Ionicons.glyphMap,
@@ -9,18 +10,19 @@ interface Props {
 
 export default function FloatingCircleButton({ icon, onPress }: Props) {
     return (
-        <TouchableOpacity style={tw(
-            'bg-accent rounded-full p-4',
-            'absolute z-1 bottom-3 right-3',
-            'shadow-lg', { shadowOpacity: 0.25 }
-        )} onPress={onPress}>
-            <Ionicons
-                name={icon}
-                style={tw(
-                    'text-3xl text-white leading-none',
-                    { transform: [{ translateX: 2 }, { translateY: 1 }] }
-                )}
-            />
-        </TouchableOpacity>
+        <TouchableScale onPress={onPress} containerStyle={tw('absolute z-1 bottom-3 right-3')}>
+            <View style={tw(
+                'bg-accent rounded-full p-4',
+                'shadow-lg', { shadowOpacity: 0.25 }
+            )}>
+                <Ionicons
+                    name={icon}
+                    style={tw(
+                        'text-3xl text-white leading-none',
+                        { transform: [{ translateX: 2 }, { translateY: 1 }] }
+                    )}
+                />
+            </View>
+        </TouchableScale>
     );
 }
