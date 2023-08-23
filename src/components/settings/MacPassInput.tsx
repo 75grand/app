842de01348cdacff78dedb50065b82eb54068ee9
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-23 11:06:55 -0700

    Fix alignment of scan MacPass button

--- src/components/settings/MacPassInput.tsx
+++ src/components/settings/MacPassInput.tsx
@@ -1,14 +1,20 @@
 import { Ionicons } from '@expo/vector-icons';
-import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View } from 'react-native';
 import tw, { color } from '../../lib/tailwind';
 import Input, { InputProps } from '../Input';
-import { useNavigation } from '@react-navigation/native';
+import TouchableScale from '../TouchableScale';
 
 export default function MacPassInput(props: InputProps) {
     const navigation = useNavigation();
 
+    function handlePress() {
+        // @ts-expect-error
+        navigation.navigate('ScanMacPass', { setMacPass: props.setValue })
+    }
+
     return (
-        <>
+        <View style={tw('relative items-end justify-center')}>
             <Input
                 {...props}
                 placeholder="Scan or enter manually"
@@ -17,13 +23,11 @@ export default function MacPassInput(props: InputProps) {
                 returnKeyType="done"
             />
 
-            <TouchableOpacity
-                style={tw('absolute px-3 right-0 top-1.75')}
-                // @ts-expect-error
-                onPress={() => navigation.navigate('ScanMacPass', { setMacPass: props.setValue })}
-            >
-                <Ionicons name="camera" size={22} color={color('accent')}/>
-            </TouchableOpacity>
-        </>
+            <TouchableScale containerStyle={tw('absolute px-3 h-full')} onPress={handlePress}>
+                <View style={tw('h-full justify-center')}>
+                    <Ionicons name="camera" size={22} color={color('accent')}/>
+                </View>
+            </TouchableScale>
+        </View>
     );
 }
