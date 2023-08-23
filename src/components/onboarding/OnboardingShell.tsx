31641f8c8b97ac772d439f457c801734ae728846
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-22 21:53:04 -0700

    Use KeyboardAvoidingView in onboarding screens

--- src/components/onboarding/OnboardingShell.tsx
+++ src/components/onboarding/OnboardingShell.tsx
@@ -1,6 +1,6 @@
 import { useStore } from '@nanostores/react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, Text, View } from 'react-native';
+import { KeyboardAvoidingView, SafeAreaView, ScrollView, Text, View } from 'react-native';
 import tw from '../../lib/tailwind';
 import { $user } from '../../lib/user/user-store';
 import Button from '../Button';
@@ -42,43 +42,49 @@ export default function OnboardingShell({
             <StatusBar animated style="dark"/>
 
             <SafeAreaView style={tw('h-full bg-white')}>
-                <View style={tw('p-6 bg-white h-full justify-between')}>
-                    <View style={tw('gap-12 items-center')}>
-                        <Logo/>
+                <KeyboardAvoidingView behavior="padding">
+                    <View style={tw('p-6 bg-white h-full justify-between')}>
+                        <View style={tw('gap-6 items-center shrink')}>
+                            <Logo/>
 
-                        <Text style={tw('text-3xl font-bold text-center')}>
-                            {title ? title : user.name ? (
-                                <>Welcome, <Text style={tw('text-accent')}>{user.name.split(' ')[0]}</Text>!</>
-                            ) : (
-                                <>Welcome!</>
+                            <ScrollView style={tw('w-full')} bounces={false} showsVerticalScrollIndicator={false}>
+                                <View style={tw('pt-6 gap-12')}>
+                                    <Text style={tw('text-3xl font-bold text-center')}>
+                                        {title ? title : user.name ? (
+                                            <>Welcome, <Text style={tw('text-accent')}>{user.name.split(' ')[0]}</Text>!</>
+                                        ) : (
+                                            <>Welcome!</>
+                                        )}
+                                    </Text>
+
+                                    <View style={tw('gap-6')}>
+                                        {children}
+                                    </View>
+                                </View>
+                            </ScrollView>
+                        </View>
+
+                        <View style={tw('gap-2 pt-3')}>
+                            <Button
+                                text={primaryButtonText}
+                                size="mega"
+                                loading={isPrimaryLoading}
+                                disabled={!isPrimaryValid}
+                                onPress={onPressPrimary}
+                            />
+
+                            {onPressSecondary && (
+                                <Button
+                                    text={secondaryButtonText}
+                                    color="light"
+                                    loading={isSecondaryLoading}
+                                    disabled={!isSecondaryValid}
+                                    onPress={onPressSecondary}
+                                />
                             )}
-                        </Text>
-
-                        <View style={tw('w-full gap-6')}>
-                            {children}
                         </View>
                     </View>
-
-                    <View style={tw('gap-2')}>
-                        <Button
-                            text={primaryButtonText}
-                            size="mega"
-                            loading={isPrimaryLoading}
-                            disabled={!isPrimaryValid}
-                            onPress={onPressPrimary}
-                        />
-
-                        {onPressSecondary && (
-                            <Button
-                                text={secondaryButtonText}
-                                color="light"
-                                loading={isSecondaryLoading}
-                                disabled={!isSecondaryValid}
-                                onPress={onPressSecondary}
-                            />
-                        )}
-                    </View>
-                </View>
+                </KeyboardAvoidingView>
             </SafeAreaView>
         </>
     );
