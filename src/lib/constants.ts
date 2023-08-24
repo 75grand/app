8af96cd03e41c893a23c00bed8e06282450cbe53
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-24 02:12:01 -0700

    Change API domain

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,5 +2,5 @@ import { isDevice } from 'expo-device';
 import { Platform } from 'react-native';
 
 export const SITE = (isDevice || Platform.OS === 'android')
-    ? 'https://api.75grand.net'
+    ? 'https://75grand.net'
     : 'http://localhost:8000';
