a995ab5897b61ed49fc3af6f7681f11606866f7c
Jerome Paulos <jeromepaulos@icloud.com>
2023-09-30 01:15:31 -0500

    Update Moodle schema

--- src/lib/types/moodle.ts
+++ src/lib/types/moodle.ts
@@ -3,8 +3,10 @@ import { zDateTime } from './utils';
 
 export type MoodleTask = z.infer<typeof MoodleTask>;
 export const MoodleTask = z.object({
-    id: z.string(),
+    id: z.number(),
     title: z.string(),
     due: zDateTime,
-    class: z.string()
+    class: z.string(),
+    description: z.string().nullable(),
+    completed_at: zDateTime.nullable()
 });
