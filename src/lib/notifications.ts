d742331dbd1c28bf1bc5186148bcdfa184ea11ba
Jerome Paulos <jeromepaulos@icloud.com>
2023-10-28 02:05:51 -0500

    Remove unused imports

--- src/lib/notifications.ts
+++ src/lib/notifications.ts
@@ -3,11 +3,8 @@ import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import { Linking, Platform } from 'react-native';
 import { patchUser } from './api/api';
-import { color } from './tailwind';
-import { $user } from './user/user-store';
 import { isLoggedIn } from './api/login';
-import { LinkingOptions } from '@react-navigation/native';
-import * as ExpoLinking from 'expo-linking';
+import { color } from './tailwind';
 
 /**
  * Set the notification handler
